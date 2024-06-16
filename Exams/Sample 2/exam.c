fd89ec650ab816006225677d7b9bbc292d31e13a
Alessio <alessioc.developer@gmail.com>
2024-06-16 23:15:18 +0200

    Completed exam 2

--- Exams/Sample 2/exam.c
+++ Exams/Sample 2/exam.c
@@ -19,55 +19,41 @@
 
 /**** BEGIN of EXERCISE #1 ****/
 
-size_t upo_bst_subtree_count_skip_2_impl(upo_bst_node_t *node)
-{
-    if (!node) { return 0; }
+size_t upo_bst_subtree_count_even_impl(upo_bst_node_t *node) {
+    if (!node) return 0;
     size_t count = 1;
+
     if (node->left) {
-        count += upo_bst_subtree_count_skip_2_impl(node->left->left)
-            + upo_bst_subtree_count_skip_2_impl(node->left->right);
+        count += upo_bst_subtree_count_even_impl(node->left->left)
+                + upo_bst_subtree_count_even_impl(node->left->right);
     }
+    
     if (node->right) {
-        count += upo_bst_subtree_count_skip_2_impl(node->right->left)
-            + upo_bst_subtree_count_skip_2_impl(node->right->right);
+        count += upo_bst_subtree_count_even_impl(node->right->left)
+                + upo_bst_subtree_count_even_impl(node->right->right);
     }
+
     return count;
 }
 
-size_t upo_bst_subtree_count_even(const upo_bst_t bst, const void *key)
-{
-    if (!bst) {
-        return 0;
-    }
-    if (upo_bst_is_empty(bst)) {
-        return 0;
-    }
+size_t upo_bst_subtree_count_even(const upo_bst_t bst, const void *key) {
+    if (!bst || upo_bst_is_empty(bst)) return 0;
 
     upo_bst_node_t *node = bst->root;
-    upo_bst_comparator_t cmp = upo_bst_get_comparator(bst);
     int res;
     int even = 1;
-    while (node && (res = cmp(key, node->key)) != 0)
-    {
-        node = res < 0 ? node->left : node->right;
+
+    while (node && (res = bst->key_cmp(node->key, key)) != 0) {
+        node = res > 0 ? node->left : node->right;
         even = !even;
     }
-    if (!node)
-    {
-        return 0;
-    }
-    if (even)
-    {
-        return upo_bst_subtree_count_skip_2_impl(node);
-    }
+
+    if (!node) return 0;
+    if (even) return upo_bst_subtree_count_even_impl(node);
 
     size_t count = 0;
-    if (node->left) {
-        count += upo_bst_subtree_count_skip_2_impl(node->left);
-    }
-    if (node->right) {
-        count += upo_bst_subtree_count_skip_2_impl(node->right);
-    }
+    if (node->left) { count += upo_bst_subtree_count_even_impl(node->left); } 
+    if (node->right) { count += upo_bst_subtree_count_even_impl(node->right); }
     return count;
 }
 
@@ -76,6 +62,33 @@ size_t upo_bst_subtree_count_even(const upo_bst_t bst, const void *key)
 
 /**** BEGIN of EXERCISE #2 ****/
 
+void upo_ht_sepchain_odelete(upo_ht_sepchain_t ht, const void *key, int destroy_data) {
+    if (ht != NULL || ht->slots != NULL) {
+        size_t h = ht->key_hash(key, ht->capacity);
+        upo_ht_sepchain_list_node_t *n = ht->slots[h].head;
+        upo_ht_sepchain_list_node_t *p = NULL;
+
+        while (n != NULL && ht->key_cmp(key, n->key)) {
+            p = n;
+            n = n->next;
+        }
+
+        if (n != NULL) {
+            if (p == NULL)
+                ht->slots[h].head = n->next;
+            else
+                p->next = n->next;
+
+            if (destroy_data) {
+                free(n->key);
+                free(n->value);
+            }
+            ht->size--;
+            free(n);
+        }
+    }
+}
+/* 
 void upo_ht_sepchain_odelete(upo_ht_sepchain_t ht, const void *key, int destroy_data)
 {
     if (!ht)
@@ -107,5 +120,5 @@ void upo_ht_sepchain_odelete(upo_ht_sepchain_t ht, const void *key, int destroy_
         }
     }
 }
-
+ */
 /**** END of EXERCISE #2 ****/

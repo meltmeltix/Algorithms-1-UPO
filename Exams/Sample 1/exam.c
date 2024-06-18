cdff5bf1f95c7a833e0278040d1608334e5d9a91
Alessio <alessioc.developer@gmail.com>
2024-06-18 16:43:22 +0200

    Removed Body of Sample Exams

--- Exams/Sample 1/exam.c
+++ Exams/Sample 1/exam.c
@@ -17,58 +17,16 @@
 
 /**** BEGIN of EXERCISE #1 ****/
 
-static const void *upo_bst_predeccessor_impl(const upo_bst_node_t *node, const void *key, upo_bst_comparator_t cmp) {
-    if (!node) return NULL; 
-    if (cmp(key, node->key) <= 0) return upo_bst_predeccessor_impl(node->left, key, cmp);
-    else {
-        const void *right = upo_bst_predeccessor_impl(node->right, key, cmp);
-        return right ? right : node->key;
-    }
-}
-
 const void *upo_bst_predecessor(const upo_bst_t bst, const void *key) {
-    if (bst == NULL || bst->root == NULL) return NULL;
-    return upo_bst_predeccessor_impl(bst->root, key, upo_bst_get_comparator(bst));
+
 }
 
 /**** END of EXERCISE #1 ****/
 
 /**** BEGIN of EXERCISE #2 ****/
 
-void upo_swap(unsigned char *a, unsigned char *b, size_t size) {
-    unsigned char *aux = malloc(size);
-    assert(aux != NULL);
-
-    memcpy(aux,  a, size);
-    memcpy(a, b, size);
-    memcpy(b, aux, size);
-
-    free(aux);
-}
-
 void upo_bidi_bubble_sort(void *base, size_t n, size_t size, upo_sort_comparator_t cmp) {
-    assert(base != NULL);
-
-    unsigned char *ptr = base;
-    bool swapped = false;
-
-    do {
-        swapped = false;
-
-        // From left to right
-        for (size_t i = 0; i < n - 1; ++i)
-            if (cmp(ptr + i * size, ptr + (i + 1) * size) > 0) { 
-                upo_swap(ptr + i * size, ptr + (i + 1) * size, size);
-                swapped = true;
-            }
-
-        // From right to left
-        for (size_t i = n - 1; i > 1; --i)
-            if (cmp(ptr + i * size, ptr + (i - 1) * size) < 0) { 
-                upo_swap(ptr + i * size, ptr + (i - 1) * size, size);
-                swapped = true;
-            }
-    } while(swapped);
+    
 }
 
 /**** END of EXERCISE #2 ****/
--- Exams/Sample 2/exam.c
+++ Exams/Sample 2/exam.c
@@ -19,42 +19,8 @@
 
 /**** BEGIN of EXERCISE #1 ****/
 
-size_t upo_bst_subtree_count_even_impl(upo_bst_node_t *node) {
-    if (!node) return 0;
-    size_t count = 1;
-
-    if (node->left) {
-        count += upo_bst_subtree_count_even_impl(node->left->left)
-                + upo_bst_subtree_count_even_impl(node->left->right);
-    }
-    
-    if (node->right) {
-        count += upo_bst_subtree_count_even_impl(node->right->left)
-                + upo_bst_subtree_count_even_impl(node->right->right);
-    }
-
-    return count;
-}
-
 size_t upo_bst_subtree_count_even(const upo_bst_t bst, const void *key) {
-    if (!bst || upo_bst_is_empty(bst)) return 0;
 
-    upo_bst_node_t *node = bst->root;
-    int res;
-    int even = 1;
-
-    while (node && (res = bst->key_cmp(node->key, key)) != 0) {
-        node = res > 0 ? node->left : node->right;
-        even = !even;
-    }
-
-    if (!node) return 0;
-    if (even) return upo_bst_subtree_count_even_impl(node);
-
-    size_t count = 0;
-    if (node->left) { count += upo_bst_subtree_count_even_impl(node->left); } 
-    if (node->right) { count += upo_bst_subtree_count_even_impl(node->right); }
-    return count;
 }
 
 /**** END of EXERCISE #1 ****/
@@ -63,62 +29,7 @@ size_t upo_bst_subtree_count_even(const upo_bst_t bst, const void *key) {
 /**** BEGIN of EXERCISE #2 ****/
 
 void upo_ht_sepchain_odelete(upo_ht_sepchain_t ht, const void *key, int destroy_data) {
-    if (ht != NULL || ht->slots != NULL) {
-        size_t h = ht->key_hash(key, ht->capacity);
-        upo_ht_sepchain_list_node_t *n = ht->slots[h].head;
-        upo_ht_sepchain_list_node_t *p = NULL;
-
-        while (n != NULL && ht->key_cmp(key, n->key)) {
-            p = n;
-            n = n->next;
-        }
-
-        if (n != NULL) {
-            if (p == NULL)
-                ht->slots[h].head = n->next;
-            else
-                p->next = n->next;
-
-            if (destroy_data) {
-                free(n->key);
-                free(n->value);
-            }
-            ht->size--;
-            free(n);
-        }
-    }
+    
 }
-/* 
-void upo_ht_sepchain_odelete(upo_ht_sepchain_t ht, const void *key, int destroy_data)
-{
-    if (!ht)
-    {
-        perror("Uninitialized hash table");
-        return;
-    }
-    upo_ht_hasher_t h = upo_ht_sepchain_get_hasher(ht);
-    size_t i = h(key, upo_ht_sepchain_capacity(ht));
-    if (ht->slots[i].head)
-    {
-        upo_ht_comparator_t cmp = upo_ht_sepchain_get_comparator(ht);
-        upo_ht_sepchain_list_node_t **before = &ht->slots[i].head;
-        while (*before && cmp((*before)->key, key) != 0)
-        {
-            before = &(*before)->next;
-        }
-        if (*before)
-        {
-            upo_ht_sepchain_list_node_t *next = (*before)->next;
-            if (destroy_data)
-            {
-                free((*before)->key);
-                free((*before)->value);
-            }
-            free(*before);
-            *before = next;
-            --ht->size;
-        }
-    }
-}
- */
+
 /**** END of EXERCISE #2 ****/
